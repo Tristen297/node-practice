510f3155bb982333838d853c490f179e33ba4b47
Tristen Dean <103838311+Tristen297@users.noreply.github.com>
2025-05-22 21:14:00 -0400

    initial commit

---
+++ global.js
@@ -0,0 +1,18 @@
+//global object
+
+//console.log(global);
+
+/*
+global.setTimeout(() => {
+    console.log('in timeout');
+    clearInterval(int);
+}, 3000);
+
+const int = setInterval(() => {
+    console.log('in the interval');
+}, 1000);
+*/
+
+//console.log(__dirname);
+//console.log(__filename);
+
